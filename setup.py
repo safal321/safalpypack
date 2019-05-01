879c44056de9867d2d56c5186cc31da82f82269c
Safal Sigdel <safalsigdel@gmail.com>
2019-05-01 09:46:14 +0545

    changes on setup

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
     name="safalpypack",
     version="0.0.1",
-    scripts=["firstpack12"]
+    author="Safal Sigdel",
+    author_email="safalsigdel@gmail.com",
+    description="A simple pip package",
+    long_description=readme,
+    url="https://github.com/safal321/safalpypack",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
 )
